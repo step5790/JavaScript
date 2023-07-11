719c149d6517875ef1c4f73722f5227fc60eda19
Stephen <stephen.s.espinosa@gmail.com>
2023-07-11 21:42:45 +0200

    file modify index.js

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -1,4 +1,22 @@
+/*
 const age = 23;
-age >= 18
-  ? console.log("I like to drink wine")
-  : console.log("I like to drink water");
+// age >= 18
+//   ? console.log("I like to drink wine")
+//   : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+//we can have template conditionals inside the template literal
+let drink2;
+
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/

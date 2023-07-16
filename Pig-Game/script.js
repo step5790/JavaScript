67d9f2123715da390cd152bacddc44e22a5866e5
Stephen <stephen.s.espinosa@gmail.com>
2023-07-16 16:03:25 +0200

    modify finalizing js in the game

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -10,26 +10,30 @@ const score1El = document.querySelector("#score--1");
 const currentScore0El = document.querySelector("#current--0");
 const currentScore1El = document.querySelector("#current--1");
 
+let currentScore, activePlayer, playingGame, scores;
+
 // starting condition
-
-let currentScore = 0;
-let activePlayer = 0;
-let playingGame = true;
-const scores = [0, 0];
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
-
-btnNew.addEventListener("click", function () {
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  playingGame = true;
+  scores = [0, 0];
   score0El.textContent = 0;
   score1El.textContent = 0;
+  currentScore0El.textContent = 0;
   currentScore1El.textContent = 0;
-  currentScore1El.textContent = 0;
-  document.querySelector(".player--0").remove("player--winner");
-  document.querySelector(".player--0").add("player--active");
-  document.querySelector(".player--1").remove("player--winner");
-  document.querySelector(".player--1").remove("player--active");
-});
+
+  diceEl.classList.add("hidden");
+  document.querySelector(".player--0").classList.remove("player--winner");
+  document.querySelector(".player--0").classList.add("player--active");
+  document.querySelector(".player--1").classList.remove("player--winner");
+  document.querySelector(".player--1").classList.remove("player--active");
+};
+
+init();
+
+// new game
+btnNew.addEventListener("click", init);
 
 // rolling dice
 btnRoll.addEventListener("click", function () {
@@ -75,6 +79,7 @@ btnHold.addEventListener("click", function () {
     );
 
     score += currentScore;
+    scores[activePlayer] = score;
     document.querySelector(`#score--${activePlayer}`).textContent = score;
 
     // check if the score is >= 100

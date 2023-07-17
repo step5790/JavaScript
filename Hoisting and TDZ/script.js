01e0a0d5a6f02f8ec4e9fe0088dc2d6b6b3c9dff
Stephen <stephen.s.espinosa@gmail.com>
2023-07-17 23:17:05 +0200

    section add Hoisting and TDZ

---
+++ Hoisting and TDZ/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var me = "Jonas";
+let job = "Teacher";
+const year = 1991;
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
